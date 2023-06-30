d0ad33f4dc3e1f1340de5cc916587b17ead14c60
DarkDragonVMC <85390502+DarkDragonVMC@users.noreply.github.com>
2023-06-30 07:57:22 +0200

    Conversion Done

--- converson.py
+++ converson.py
@@ -1,15 +1,23 @@
 from PIL import Image
 
-input = Image.new('L', [1920, 1080])
-
+input = Image.new("RGB", (100, 100))
 pixel_map = input.load()
 width, height = input.size
 
-for i in range(width//2):
+time = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+amplitude = (20, 40, 30, 67, 21, 98, 83, 78, 65, 68)
+frequency = (440, 450, 480, 510, 430, 400, 380, 350, 450, 420)
+
+for i in range(width):
     for j in range(height):
-        r,g,b = input.getpixel(i, j)
-        grayscale = (.299*r, .587*g, .114*b)
-        pixel_map[i, j] = (int(grayscale, int(grayscale, int(grayscale))))
+        pixel_map[i, j] = 255, 255, 255
+
+for i in range(len(time)):
+    for j in range(len(amplitude)):
+        for k in range(len(frequency)):
+            pixel_map[time[i], amplitude[j]] = (frequency[k] % 255, amplitude[j] % 255, (amplitude[j]*frequency[k]) % 255)
+
+
 
 
 input.save ('grayscale', format = 'png')
